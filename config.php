ee773654e0e7f4f4dc5f19ff33b290023cc69a19
novakvova <novakvova@gmail.com>
2021-11-21 11:41:56 +0200

    Add and show news

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME','local.vpu911.com');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+define('DB_CHARSET','utf8');
+define('DB_DRIVER','mysql');
+?>
---
+++ connection_database.php
@@ -0,0 +1,13 @@
+<?php
+require_once "config.php";
+try {
+    $dbh = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME,
+        DB_USER, DB_PASSWORD,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ".DB_CHARSET));
+    //seedAuto($dbh);
+    //$arr["slavik"]=12;
+
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit();
+}
